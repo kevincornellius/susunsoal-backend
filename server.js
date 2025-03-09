36e2fd8ad5a03c12e3cb4174edaeae5eaba2ac28
kevincornellius <kevinbananax@gmail.com>
2025-03-09 15:17:56 +0700

    Fix Vercel timeout issue

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.listen(5000, () => console.log("Server running on port 5000"));
 module.exports = app;
 
 if (require.main === module) {
-  app.listen(PORT, () => {
+  app.listen(5000, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
